9c629d118f19654ddeee6c9b43648835579e06b8
imps252 <145042089+imps252@users.noreply.github.com>
2025-01-22 19:07:21 +0530

    sucess

--- src/wipro.java
+++ src/wipro.java
@@ -3,6 +3,7 @@ public class wipro {
 
 	public static void main(String[] args) {
 		System.out.println("welcome wipro");
+		System.out.println("number 1 is odd");
 		// TODO Auto-generated method stub
 
 	}
