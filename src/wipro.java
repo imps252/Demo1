ef0b384bc88f9ae8ac92a541e0e245f2a8ba9024
imps252 <145042089+imps252@users.noreply.github.com>
2025-01-22 19:16:51 +0530

    Update wipro.java

--- src/wipro.java
+++ src/wipro.java
@@ -4,6 +4,8 @@ public class wipro {
 	public static void main(String[] args) {
 		System.out.println("welcome wipro");
 		System.out.println("number 1 is odd");
+		System.out.println("number 2 is even");
+		
 		// TODO Auto-generated method stub
 
 	}
